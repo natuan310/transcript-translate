c056f33077ed5bbd6a68b4c39f7eab6608eabe60
natuan310 <natuan310@gmail.com>
2020-11-10 17:41:44 +0700

    re-config

---
+++ public/index.js
@@ -0,0 +1,92 @@
+const socketio = io();
+const socket = socketio.on('connect', function () {
+    // reset the recorder
+    console.log('Connected')
+    startRecording.disabled = false;
+});
+
+// when the server found results and send
+// it back to the client
+const resultpreview = document.getElementById('results');
+socketio.on('transcript', function (data) {
+    if (data && data.results[0] && data.results[0].alternatives[0]) {
+        resultpreview.innerHTML += data.results[0].alternatives[0].transcript + "\n";
+    }
+});
+
+const translation = document.getElementById('translation');
+const re_translation = document.getElementById('re_translation');
+socketio.on('translate', function (data) {
+    translation.innerHTML += data.translation + "\n"
+    re_translation.innerHTML += data.re_translation + "\n"
+});
+
+const startRecording = document.getElementById('start-recording');
+const stopRecording = document.getElementById('stop-recording');
+let recordAudio;
+
+// on start button handler
+startRecording.onclick = function () {
+    // recording started
+    startRecording.disabled = true;
+
+    // make use of HTML 5/WebRTC, JavaScript getUserMedia()
+    // to capture the browser microphone stream
+    navigator.getUserMedia({
+        audio: true
+    }, function (stream) {
+        recordAudio = RecordRTC(stream, {
+            type: 'audio',
+            mimeType: 'audio/webm',
+            sampleRate: 44100, // this sampleRate should be the same in your server code
+
+            // MediaStreamRecorder, StereoAudioRecorder, WebAssemblyRecorder
+            // CanvasRecorder, GifRecorder, WhammyRecorder
+            recorderType: StereoAudioRecorder,
+
+            // Dialogflow / STT requires mono audio
+            numberOfAudioChannels: 1,
+
+            // get intervals based blobs
+            // value in milliseconds
+            // as you might not want to make detect calls every seconds
+            timeSlice: 4000,
+
+            // only for audio track
+            // audioBitsPerSecond: 128000,
+
+            // used by StereoAudioRecorder
+            // the range 22050 to 96000.
+            // let us force 16khz recording:
+            desiredSampRate: 16000,
+
+            // as soon as the stream is available
+            ondataavailable: function (blob) {
+                // making use of socket.io-stream for bi-directional
+                // streaming, create a stream
+                var stream = ss.createStream();
+                // stream directly to server
+                // it will be temp. stored locally
+                ss(socket).emit('stream-transcribe', stream, {
+                    name: 'stream.wav',
+                    size: blob.size
+                });
+                // pipe the audio blob to the read stream
+                ss.createBlobReadStream(blob).pipe(stream);
+            }
+        });
+
+        recordAudio.startRecording();
+        stopRecording.disabled = false;
+    }, function (error) {
+        console.error(JSON.stringify(error));
+    });
+};
+
+// on stop button handler
+stopRecording.onclick = function () {
+    // recording stopped
+    startRecording.disabled = false;
+    stopRecording.disabled = true;
+};
+
---
+++ server.js
@@ -0,0 +1,236 @@
+require('dotenv').config();
+const projectId = process.env.PROJECT_ID;
+const port = process.env.PORT || 3000;
+
+const languageCode = 'en-US';
+let encoding = 'LINEAR16';
+
+const singleUtterance = true;
+const interimResults = false;
+const sampleRateHertz = 16000;
+const speechContexts = [
+  {
+    phrases: [
+      'mail',
+      'email'
+    ],
+    boost: 20.0
+  }
+]
+
+// console.log(projectId);
+
+// ----------------------
+
+
+// load all the libraries for the server
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io')
+const ss = require('socket.io-stream');
+
+const app = express();
+var server;
+// var sessionId, sessionClient, sessionPath, request;
+// var speechClient, requestSTT, ttsClient, requestTTS, mediaTranslationClient, requestMedia;
+var speechClient, requestSTT;
+// Set static folder
+app.use(express.static(path.join('public')));
+
+
+// STT demo
+const speech = require('@google-cloud/speech');
+
+function setupServer() {
+  // setup Express
+  app.use(cors());
+  app.get('/', function (req, res) {
+    res.render(path.join('index.html'));
+  });
+  server = http.createServer(app);
+  const io = socketIo(server);
+  server.listen(process.env.PORT || 3000, () => {
+    console.log('Server started!');
+  });
+
+  // Listener, once the client connect to the server socket
+  io.on('connect', (client) => {
+    console.log(`Client connected [id=${client.id}]`);
+    client.emit('server_setup', `Server connected [id=${client.id}]`);
+
+    // // when the client sends 'message' events
+    // // when using simple audio input
+    // client.on('message', async function(data) {
+    //     // we get the dataURL which was sent from the client
+    //     const dataURL = data.audio.dataURL.split(',').pop();
+    //     // we will convert it to a Buffer
+    //     let fileBuffer = Buffer.from(dataURL, 'base64');
+    //     // run the simple detectIntent() function
+    //     const results = await detectIntent(fileBuffer);
+    //     client.emit('results', results);
+    // });
+
+    // // when the client sends 'message' events
+    // // when using simple audio input
+    //   client.on('message-transcribe', async function(data) {
+    //     // we get the dataURL which was sent from the client
+    //     const dataURL = data.audio.dataURL.split(',').pop();
+    //     // we will convert it to a Buffer
+    //     let fileBuffer = Buffer.from(dataURL, 'base64');
+    //     // run the simple transcribeAudio() function
+    //     const results = await transcribeAudio(fileBuffer);
+    //     client.emit('results', results);
+    // });
+
+    // // when the client sends 'stream' events
+    // // when using audio streaming
+    // ss(client).on('stream', function(stream, data) {
+    //   // get the name of the stream
+    //   const filename = path.basename(data.name);
+    //   // pipe the filename to the stream
+    //   stream.pipe(fs.createWriteStream(filename));
+    //   // make a detectIntStream call
+    //   detectIntentStream(stream, function(results){
+    //       console.log(results);
+    //       client.emit('results', results);
+    //   });
+    // });
+
+    // when the client sends 'stream-transcribe' events
+    // when using audio streaming
+    ss(client).on('stream-transcribe', function (stream, data) {
+      // get the name of the stream
+      // const filename = path.basename(data.name);
+      // pipe the filename to the stream
+      // stream.pipe(fs.createWriteStream(filename));
+      // make a detectIntStream call
+      transcribeAudioStream(stream, async function (results) {
+        // console.log(results['results'][0]['alternatives'][0].transcript)
+        client.emit('transcript', results);
+
+        transcript = results['results'][0]['alternatives'][0].transcript
+        let res = await eng2jap(transcript);
+        // console.log(res)
+        client.emit('translate', res)
+      });
+
+
+    });
+
+    // // when the client sends 'tts' events
+    // ss(client).on('tts', function(text) {
+    //   textToAudioBuffer(text).then(function(results){
+    //     console.log(results);
+    //     client.emit('results', results);
+    //   }).catch(function(e){
+    //     console.log(e);
+    //   });
+    // });
+
+    // when the client sends 'stream-media' events
+    // when using audio streaming
+    // ss(client).on('stream-media', function (stream, data) {
+    //   // get the name of the stream
+    //   const filename = path.basename(data.name);
+    //   // pipe the filename to the stream
+    //   stream.pipe(fs.createWriteStream(filename));
+    //   // make a detectIntStream call
+    //   transcribeAudioMediaStream(stream, function (results) {
+    //     console.log(results);
+    //     client.emit('results', results);
+    //   });
+    // });
+  });
+}
+
+
+/**
+ * Setup Cloud STT Integration
+ */
+function setupSTT() {
+  // Creates a client
+  speechClient = new speech.SpeechClient();
+
+  // Create the initial request object
+  // When streaming, this is the first call you will
+  // make, a request without the audio stream
+  // which prepares Dialogflow in receiving audio
+  // with a certain sampleRateHerz, encoding and languageCode
+  // this needs to be in line with the audio settings
+  // that are set in the client
+  requestSTT = {
+    config: {
+      sampleRateHertz: sampleRateHertz,
+      encoding: encoding,
+      languageCode: languageCode
+    },
+    interimResults: interimResults,
+    //enableSpeakerDiarization: true,
+    //diarizationSpeakerCount: 2,
+    //model: `phone_call`
+  }
+}
+
+/*
+ * STT - Transcribe Speech on Audio Stream
+ * @param audio stream
+ * @param cb Callback function to execute with results
+ */
+async function transcribeAudioStream(audio, cb) {
+  const recognizeStream = speechClient.streamingRecognize(requestSTT)
+    .on('data', function (data) {
+      // console.log(data);
+
+      // let res = data['results'][0]['alternatives'][0].transcript;
+      // console.log(res);
+
+      cb(data);
+    })
+    .on('error', (e) => {
+      console.log(e);
+    })
+    .on('end', () => {
+      console.log('on end');
+    });
+
+  audio.pipe(recognizeStream);
+  audio.on('end', function () {
+    //fileWriter.end();
+  });
+};
+
+
+// Imports the Google Cloud client library
+const { Translate } = require('@google-cloud/translate').v2;
+
+// Instantiates a client
+const translate = new Translate({ projectId });
+
+// Function to translate text
+async function eng2jap(sourceText) {
+
+  // let sourceText = transcript[0].alternatives[0].transcript;
+  // console.log(sourceText);
+  // The target language
+  const source_lang = 'en';
+  const target_lang = 'ja';
+
+  // Translates some text into Japanese
+  const [translation] = await translate.translate(sourceText, target_lang);
+  const [re_translation] = await translate.translate(translation, source_lang);
+  // console.log(`Source: ${sourceText}`);
+  // console.log(`Japanese: ${translation}`);
+  // console.log(`Re-translate: ${re_translation}`);
+  // socket.emit('transcript', {
+  //     'translation': translation,
+  //     're_translation': re_translation
+  // });
+  return { translation, re_translation };
+}
+
+
+setupServer();
+setupSTT();
